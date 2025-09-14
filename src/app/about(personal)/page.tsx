fe171a4b62e6b2c0ba51e6ceb7f59c22e4b2ab38
Hunter Hockman <84150506+Anti-Loli@users.noreply.github.com>
2025-09-14 13:53:45 -0400

    Website skeleton and gradient background

---
+++ src/app/about(personal)/page.tsx
@@ -0,0 +1,10 @@
+
+export default function AboutPersonal(){
+      return (
+    <div>
+     
+      About (personal) Page
+        
+    </div>
+  );
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,6 @@ export default function About(){
     <div>
      
       About Page
-      
         
     </div>
   );
