344ce44d31cb97c937bca496de56beba5a8471a6
Hunter Hockman <84150506+Anti-Loli@users.noreply.github.com>
2025-11-14 14:01:49 -0500

    Update Profile.tsx
    Coded basic layout for About Page Profile

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -2,26 +2,23 @@ import Button from "../components/Button";
 
 export default function XboxProfile({ username }: { username: string }) {
   return (
-    <div className="flex flex-col items-center justify-center p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition-shadow duration-300 w-64">
-
-      <div className="flex flex-row gap-2 mb-4">
-        <Button className="w-12 h-8 bg-[#107C10] text-white rounded-lg cursor-pointer text-base text-base block m-3">
-          .
-        </Button>
-
-        <Button href = "/about(personal)">
-          .
-        </Button>
-      </div>
-     
-
-      <div className="flex flex-row gap-2 mb-4">      
-        Green box
-
-        Profile box
-      </div>
-
-    </div>
+    <main className="justify-center items-center h-screen mx-auto flex gap-x-4 rounded-xl bg-white p-6 shadow-lg">
+        <div className="grid grid-flow-row grid-rows-4 gap-4 justify-items-center -mr-2"> 
+          <div className="col-span-18 px-5"> 
+            <Button> Professional </Button>
+            </div> 
+            <div className="col-span-18 outline-2 px-12">List of stuff</div> 
+            <div className="col-span-18 outline-2 px-10">Education info</div> 
+            </div> 
+            
+            <div className="grid grid-flow-row grid-rows-4 gap-4 justify-items-center -ml-2"> 
+              <div className="col-span-16 px-16"> 
+                <Button href = "/about(personal)"> Personal </Button> 
+                </div> 
+            <div className="col-span-15 outline-2 grid grid-flow-col grid-cols-2"> Name Profession Current Job </div> 
+            <div className="col-span-16 outline-2 px-16">Tools List</div> 
+        </div> 
+      </main>
 
 
   );
